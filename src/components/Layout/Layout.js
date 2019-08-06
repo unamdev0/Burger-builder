2648fb6adb1fa207b58396c569211d40cc6e16da
Udit Namdev <unamdev0@gmail.com>
2019-08-06 22:00:10 +0530

    Changes made to improve perfomance

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,11 +3,10 @@ import Aux from '../../hoc/auxilary'
 import styles from './Layout.css'
 import Toolbar from '../Navigation/toolbar/toolbar'
 
-
 const Layout=(props)=>{
     return(
         <Aux>
-            <Toolbar></Toolbar>
+            <Toolbar/>
             <main className={styles.content} >
                 {props.children}
             </main>
--- src/components/Navigation/toolbar/toolbar.js
+++ src/components/Navigation/toolbar/toolbar.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import styles from './toolbar.css'
-import NavigationItems from '../../UI/NavigationItems/Navigationitems'
+ import NavigationItems from '../NavigationItems/Navigationitems'
 
 const toolbar =(props)=>(
     <header className={styles.toolbar}>
-        <NavigationItems/>
+        
         <div>Menu</div>
         <div>Logo</div>
         <nav>
-            ...
+        <NavigationItems/>
         </nav>
     </header>
 )
